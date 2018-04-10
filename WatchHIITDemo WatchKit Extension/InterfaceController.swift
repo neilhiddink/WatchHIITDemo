1e1dc236f7f23ee96b7d7c2d52851c8831172588
Neil Hiddink <nhiddink2@gmail.com>
2018-04-10 09:47:33 -0400

    feat: Add Example for Repeating Timer without using WKInterfaceTimer

--- WatchHIITDemo WatchKit Extension/InterfaceController.swift
+++ WatchHIITDemo WatchKit Extension/InterfaceController.swift
@@ -37,7 +37,7 @@ class InterfaceController: WKInterfaceController {
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        intervalTime = myInterval
     }
     
     override func willActivate() {
@@ -59,6 +59,20 @@ class InterfaceController: WKInterfaceController {
         timer.setDate(time)
         timer.start()
     }
+    
+    // Format seconds into strings of hh:mm:ss
+    func formatTimeInterval(timeInterval: TimeInterval) -> String {
+        let secondsInHour = 3600
+        let secondsInMinute = 60
+        var time = Int(timeInterval)
+        let hours = time / secondsInHour
+        time = time % secondsInHour
+        let minutes = time / secondsInMinute
+        let seconds = time % secondsInMinute
+        return String(format:"%02i:%02i:%02i",hours,minutes,seconds)
+    }
+    
+    // MARK: Timers
     
     // Simple Timer Object
     func loopTimer1(interval: TimeInterval) {
@@ -88,6 +102,20 @@ class InterfaceController: WKInterfaceController {
         resetWKTimer(timer: workoutTimer, interval: 0)
     }
     
+    // Repeating Timer Object without WKInterfaceTimer
+    func loopTimer3(interval: TimeInterval, timerInterval: TimeInterval) {
+        if timer.isValid {
+            timer.invalidate()
+        }
+        timer = Timer.scheduledTimer(timeInterval: timerInterval,
+                                     target: self,
+                                     selector: #selector(loopTimer3DidEnd(timer:)),
+                                     userInfo: interval,
+                                     repeats: true)
+        resetWKTimer(timer: intervalTimer, interval: interval)
+        resetWKTimer(timer: workoutTimer, interval: 0)
+    }
+    
     @objc func loopTimer1DidEnd(timer: Timer) {
         statusLabel.setText("Stopped")
         startStopButton.setTitle("Start")
@@ -107,6 +135,26 @@ class InterfaceController: WKInterfaceController {
         }
     }
     
+    @objc func loopTimer3DidEnd(timer: Timer) {
+        // infinite loop with events in the selector
+        // uses labels instead of witch kit timers
+        workoutTime += timer.timeInterval  // increment count up timers
+        intervalTime -= timer.timeInterval // decrement count down timers
+        if intervalTime <= 0 { // the workout interval is over
+            // switch activities
+            intervalTime = timer.userInfo as! TimeInterval
+            isRunning = !isRunning
+            resetWKTimer(timer: intervalTimer, interval: timer.userInfo as! TimeInterval)
+            if isRunning {
+                statusString = "Run "
+            } else {
+                statusString = "Walk "
+            }
+        }
+        statusLabel.setText(statusString + formatTimeInterval(timeInterval: intervalTime))
+        elapsedLabel.setText(formatTimeInterval(timeInterval: workoutTime))
+    }
+    
     // MARK: IB Actions
 
     @IBAction func startStopButtonPressed() {
@@ -115,13 +163,16 @@ class InterfaceController: WKInterfaceController {
             // statusLabel.setText("Run")
             if isRunning {
                 statusLabel.setText("Run")
+                statusString = "Run "
             } else {
                 statusLabel.setText("Walk")
+                statusString = "Walk "
             }
             startStopButton.setTitle("Stop")
             
             // loopTimer1(interval: myInterval)
-            loopTimer2(interval: myInterval)
+            // loopTimer2(interval: myInterval)
+            loopTimer3(interval: myInterval, timerInterval: 0.25)
         } else {
             startStopButton.setTitle("Start")
             workoutTimer.stop()
